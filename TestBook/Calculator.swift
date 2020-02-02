5e324e561ba3a199e473f6864a4cf561f44a7da9
R.ito <kawboy442@gmail.com>
2020-02-02 21:03:46 +0900

    feat: add Calculator class

---
+++ TestBook/Calculator.swift
@@ -0,0 +1,29 @@
+//
+//  Calculator.swift
+//  TestBook
+//
+//  Created by 伊藤僚亮 on 2020/02/02.
+//  Copyright © 2020 R.Ito. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    
+    
+    func add(_ x: Int, _ y: Int) -> Int {
+        return x + y
+    }
+    
+    func substract(_ x: Int, _ y: Int) -> Int {
+        return x - y
+    }
+    
+    func nultiple(_ x: Int, _ y: Int) -> Int {
+        return x * y
+    }
+    
+    func division(_ x: Int, _ y: Int) -> Int {
+        return x / y
+    }
+}
